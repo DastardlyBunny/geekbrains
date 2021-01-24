aef862d2a6300674465e882a5952e8321cbccf36
kr0085@mail.ru <TzrV1f00sxK3e>
2021-01-24 21:45:17 +0400

    lesson4

--- src/main/java/ru/geekbrains/lesson4/MainApp.java
+++ src/main/java/ru/geekbrains/lesson4/MainApp.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class MainApp {
 
-    private static final int SIZE = 5;
+    private static final int SIZE = 3;
 
     private static final char[][] map = new char[SIZE][SIZE];
 
@@ -16,11 +16,11 @@ public class MainApp {
 
     private static final char DOT_EMPTY = '*';
 
-    private static final int WIN_COUNT = 4;
+    private static final int WIN_COUNT = 2;
 
     private static final String[] userAnswer = { "ДА", "НЕТ" };
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner;
 
     public static void main(String[] args) {
         initMap();
@@ -28,6 +28,8 @@ public class MainApp {
 
         while (true) {
             try {
+                scanner = new Scanner(System.in);
+
                 System.out.println("Введите число Y:");
                 int y = scanner.nextInt() - 1;
 
