b21c75843a0180cba4036bb02a969b59ebd74fa4
kr0085@mail.ru <TzrV1f00sxK3e>
2021-02-15 20:59:41 +0400

    lesson2_1

---
+++ src/main/java/ru/geekbrains2/lesson1/participant/Cat.java
@@ -0,0 +1,39 @@
+package ru.geekbrains2.lesson1.participant;
+
+public class Cat implements Participant {
+    private final int maxRun;
+    private final int maxJump;
+
+    public Cat(int maxRun, int maxJump) {
+        this.maxRun = maxRun;
+        this.maxJump = maxJump;
+    }
+
+    public int getMaxRun() {
+        return maxRun;
+    }
+
+    public int getMaxJump() {
+        return maxJump;
+    }
+
+    @Override
+    public boolean run(int distance) {
+        if (distance > maxRun) {
+            return false;
+        }
+        System.out.println("Cat run");
+        return true;
+    }
+
+    @Override
+    public boolean jump(int distance) {
+        if (distance > maxJump) {
+            return false;
+        }
+        System.out.println("Cat jump");
+        return true;
+    }
+
+
+}
